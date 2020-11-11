4b4b88306c6278f07adcb46d5edc25c454a29078
trnciii <doityan223@gmail.com>
2020-11-11 22:59:13 +0900

    update RenderPasses
    expose members, separate image interpretation

--- src/interface.cpp
+++ src/interface.cpp
@@ -6,6 +6,20 @@
 #include "cmp.hpp"
 #include "RenderPasses.hpp"
 #include "file.hpp"
+#include "data.hpp"
+
+
+inline std::vector<float> getImage(RenderPasses passes, const uint32_t layer){
+	const glm::vec3* im_v = passes.data(layer);
+
+	std::vector<float> f(passes.length*3);
+	for(int i=0; i<passes.length; i++){
+		f[3*i  ] = im_v[i].x;
+		f[3*i+1] = im_v[i].y;
+		f[3*i+2] = im_v[i].z;
+	}
+	return f;
+}
 
 std::vector<float> renderReference_wrap(
 	RenderPasses& passes, const int layer, const int spp, const Scene& scene)
@@ -22,15 +36,8 @@ std::vector<float> renderReference_wrap(
 
 	delete[] rngForEveryPixel;
 
-	std::vector<float> imagef(w*h*3);
-	for(int i=0; i<w*h; i++){
-		imagef[3*i  ] = result[i].x;
-		imagef[3*i+1] = result[i].y;
-		imagef[3*i+2] = result[i].z;
-	}
-	return imagef;
+	return getImage(passes, layer);
 }
-
 
 std::vector<float> renderNonTarget_wrap(
 	RenderPasses& passes, const int layer, const int spp, const Scene& scene)
@@ -47,24 +54,26 @@ std::vector<float> renderNonTarget_wrap(
 
 	delete[] rngForEveryPixel;
 
-	std::vector<float> imagef(w*h*3);
-	for(int i=0; i<w*h; i++){
-		imagef[3*i  ] = result[i].x;
-		imagef[3*i+1] = result[i].y;
-		imagef[3*i+2] = result[i].z;
-	}
-	return imagef;
+	return getImage(passes, layer);
 }
 
 BOOST_PYTHON_MODULE(composition) {
 	using namespace boost::python;
 
-	class_<std::vector<float>>("vecf")
+	class_<std::vector<float>>("vF")
 		.def(vector_indexing_suite<std::vector<float>>());
 
 	class_<RenderPasses>("RenderPasses", init<int, int>())
+		.def_readonly("width", &RenderPasses::width)
+		.def_readonly("height", &RenderPasses::height)
+		.def_readonly("nLayers", &RenderPasses::nLayer)
+		// .def("data", &RenderPasses::data)
 		.def("addLayer", &RenderPasses::addLayer)
 		.def("clear", &RenderPasses::clear);
+
+	class_<hitpoint>("hitpoint");
+	// class_<std::vector<hitpoint>>("vHit")
+		// .def(vector_indexing_suite<std::vector<hitpoint>>());
 
 	// class_<RNG>("RNG")
 	// 	.def("seed", &RNG::seed)
