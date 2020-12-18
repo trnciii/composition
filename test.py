3aabf70dc208157babe40084f0a92a2d450fa78f
trnciii <doityan223@gmail.com>
2020-12-18 20:45:31 +0900

    update test

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 import bpy
-import importlib
 import numpy as np
 import composition
-importlib.reload(composition)
 col = composition.color
 
 path = "C:\\Users\\Rinne\\Drive\\workshop\\composition\\result\\"
@@ -15,7 +13,7 @@ m2 = 'mask2'
 d1 = 'depth1'
 d2 = 'depth2'
 nt = 'nt'
-rf = 'referemce'
+rf = 'reference'
 
 def hitsToDepth(context, hits, key):
     depth = [0]*(context.renderpass.width*context.renderpass.height)
@@ -28,6 +26,18 @@ def hitsToDepth(context, hits, key):
 #        d = depth[i]/count[i] if count[i] else 0
         d = depth[i]/128
         context.renderpass.set(context.bind[key], i, d, d, d)
+
+def allHitsToImage(cmp, h1, h2, k1, k2, c):
+    cmp.hitsToImage(h1, k1, c)
+    cmp.hitsToImage(h2, k2, c)
+    
+def ppm(cmp, h1, h2):
+    R0 = 0.5
+    itr = 1000
+    nPhoton = 10000
+    alpha = 0.6
+    cmp.ppm(cmp.target1, h1, R0, itr, nPhoton, alpha)
+    cmp.ppm(cmp.target2, h2, R0, itr, nPhoton, alpha)
 
 def purity(rad, ch):
     a = (rad[0] + rad[1] + rad[2])/3
@@ -48,19 +58,6 @@ def cel(hit):
 #    return [g, g, g]
     c = [0.3, 0.1, 0.03] if dot<0 else [0.8, 0.6, 0.1]
     return [c[0], (1-g)*c[1]+g, c[2]]
-
-def mask(cmp, hits, key, nRay):
-    length = cmp.renderpass.width*cmp.renderpass.height
-    count = [0]*3*length
-
-    for i in range(hits.size()):
-        hit = hits.element(i)
-        count[hit.pixel] += 1
-
-    for i in range(length):
-        cmp.renderpass.set(cmp.bind[key], i, count[i]/nRay, 0, 0)
-    
-    cmp.copyImage(key)
     
 def setAlpha(cmp, key_color, key_alpha, key_out):
     ps = cmp.renderpass
@@ -71,59 +68,50 @@ def setAlpha(cmp, key_color, key_alpha, key_out):
 #    bpy.data.images[key_out].pixels = im
     bpy.data.images[key_out].pixels = im
     
-def depth(cmp, hits, key, nRay):
-    length = cmp.renderpass.width*cmp.renderpass.height
-    d = [0]*3*length
-    count = [0]*3*length
-    max = 0
-    for i in range(hits.size()):
-        hit = hits.element(i)
-        d[hit.pixel] += hit.depth
-        count[hit.pixel] += 1
-        max = hit.depth if hit.depth > max else max
-    for i in range(length):
-        if count[i] > 0:
-            cmp.renderpass.set(cmp.bind[key], i, d[i]/nRay, 0, 0)
-    cmp.copyImage(key)
-    return max
+def orange(hit):
+    tau = col.radiance(hit)
+    return [tau[0]*0.8, tau[1]*0.3, tau[2]*0.1]
+
+def green(hit):
+    tau = col.radiance(hit)
+    return [tau[0]*0.2, tau[1]*0.9, tau[2]*0.4]
 
 print("---- start ----")
 
-hits1_ex = composition.core.Hits()
-hits2_ex = composition.core.Hits()
+#hits1_ex = composition.core.Hits()
+#hits2_ex = composition.core.Hits()
 hits1_total = composition.core.Hits()
 hits2_total = composition.core.Hits()
 
-
 #hits1_ex.load(path + "hits1")
 #hits2_ex.load(path + "hits2")
-hits1_total.load(path + "hits1_all")
-hits2_total.load(path + "hits2_all")
+hits1_total.load(path + "hits1_total")
+hits2_total.load(path + "hits2_total")
 
 cmp = composition.Context()
 cmp.bindImage(t1)
 cmp.bindImage(t2)
 cmp.bindImage(m1)
 cmp.bindImage(m2)
+cmp.bindImage(d1)
+cmp.bindImage(d2)
+cmp.bindImage(nt)
 
-#mask(cmp, hits1_ex, m1, 512)
-mask(cmp, hits1_total, m1, 64)
-#cmp.mask(hits1_total, m1, 64)
-#cmp.mask(hits2_total, m2, 64)
+cmp.load(nt, path+"nontarget")
 
-#mask(cmp, hits2_ex, m2, 128)
-mask(cmp, hits2_total, m2, 64)
+cmp.mask(hits1_total, m1, 64)
+cmp.mask(hits2_total, m2, 64)
+
+cmp.depth(hits1_total, d1, 64)
+cmp.depth(hits2_total, d2, 64)
 
 cmp.hitsToImage(hits1_total, t1, col.normal)
 cmp.hitsToImage(hits2_total, t2, col.normal)
-#cmp.hitsToImage(hits2, t2, col.radiance)
 
-setAlpha(cmp, t1, m1, t1)
-setAlpha(cmp, t2, m2, t2)
 
-del cmp
-del hits1_ex
-del hits2_ex
-del hits1_total, hits2_total
+for name in dir():
+    if not name.startswith('_'):
+#        print(name)
+        del globals()[name]
 
 print("---- end ----")
