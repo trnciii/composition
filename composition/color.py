b273ccdfbedb2162730ffc635aaa02a7884a3711
trnciii <doityan223@gmail.com>
2020-12-18 19:40:25 +0900

    fix radiance calculation: avoid zero division

--- composition/color.py
+++ composition/color.py
@@ -1,7 +1,10 @@
 from . import core
 
 def radiance(hit):
-    return [hit.tau.x/hit.iteration, hit.tau.y/hit.iteration, hit.tau.z/hit.iteration]
+	if hit.iteration > 0:
+	    return [hit.tau.x/hit.iteration, hit.tau.y/hit.iteration, hit.tau.z/hit.iteration]
+	else:
+		return [0, 0, 0]
 
 def normal(hit):
 	return [hit.n.x, hit.n.y, hit.n.z]
