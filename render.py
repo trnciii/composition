e501ab921c66060d1ae3801658ffe25659964ba9
trnciii <doityan223@gmail.com>
2021-01-15 18:40:42 +0900

    udpate render.py: refactoring -add main()

--- render.py
+++ render.py
@@ -8,10 +8,6 @@ def ls(a):
     for i in a:
         print(i)
     print()
-
-def linfo(l):
-    print(l.from_node)
-    print(l.to_node)
     
 def terminate():
     bpy.data.scenes["Scene"].node_tree.nodes["Alpha Over"].inputs[0].default_value = 0
@@ -23,6 +19,20 @@ def terminate():
             del globals()[name]
     
     print("---- end ----")
+
+path = bpy.path.abspath('//result') + '/'
+t0 = 'target1'
+t1 = 'target2'
+t0s = 'target1s'
+t1s = 'target2s'
+m0 = 'mask1'
+m1 = 'mask2'
+d0 = 'depth1'
+d1 = 'depth2'
+nt = 'nt'
+rf = 'reference'
+tx0 = 'texture1'
+tx1 = 'texture2'
     
 ramp_green = [
     (0, [0.03, 0.1, 0.03]),
@@ -39,96 +49,91 @@ ramp_red = [
 ]
 
 
-path = bpy.path.abspath('//result') + '/'
-t0 = 'target1'
-t1 = 'target2'
-t0s = 'target1s'
-t1s = 'target2s'
-m0 = 'mask1'
-m1 = 'mask2'
-d0 = 'depth1'
-d1 = 'depth2'
-nt = 'nt'
-rf = 'reference'
-tx0 = 'texture1'
-tx1 = 'texture2'
+def context():
+    cmp = composition.bi.Context()
 
-cmp = composition.bi.Context()
-cmp.bindImage(rf)
-cmp.bindImage(nt)
-cmp.bindImage(t0)
-cmp.bindImage(t1)
+    cmp.bindImage(rf)
+    cmp.bindImage(nt)
+    cmp.bindImage(t0)
+    cmp.bindImage(t1)
 
-cmp.scene.setCamera('Camera')
+    cmp.scene.setCamera('Camera')
 
-env = composition.core.Material()
-env.type = composition.core.MtlType.emit
-env.color = composition.core.vec3(0, 0, 0)
-cmp.scene.setEnvironment(env)
+    env = composition.core.Material()
+    env.type = composition.core.MtlType.emit
+    env.color = composition.core.vec3(0, 0, 0)
+    cmp.scene.setEnvironment(env)
 
-# light
-cmp.scene.addSphere('Sphere.001')
+    # light
+    cmp.scene.addSphere('Sphere.001')
 
-# targets
-cmp.scene.addMesh('Sphere.002')
-cmp.scene.addSphere('Sphere')
+    # targets
+    cmp.scene.addMesh('Sphere.002')
+    cmp.scene.addSphere('Sphere')
 
-cmp.scene.addMesh('floor')
-#cmp.scene.addMesh('left_proxi')
-#cmp.scene.addMesh('back_proxi')
-cmp.scene.addMesh('left')
-cmp.scene.addMesh('back')
-cmp.scene.addMesh('right')
-#cmp.scene.addMesh('Suzanne')
+    cmp.scene.addMesh('floor')
+    #cmp.scene.addMesh('left_proxi')
+    #cmp.scene.addMesh('back_proxi')
+    cmp.scene.addMesh('left')
+    cmp.scene.addMesh('back')
+    cmp.scene.addMesh('right')
+    #cmp.scene.addMesh('Suzanne')
 
-cmp.scene.addTarget('target1')
-cmp.scene.addTarget('target2')
+    cmp.scene.addTarget('target1')
+    cmp.scene.addTarget('target2')
 
-cmp.scene.print()
+    cmp.scene.print()
+    return cmp
 
-time.sleep(0.2)
-time0 = time.time()
+def main():
 
-#print('pt')
-#cmp.pt_ref(rf, 500)
+    cmp = context()
 
-print('pt_nt')
-cmp.pt_nt(nt, 500)
+    param_preview = composition.core.PPMParam()
+    param_preview.nRay = 16
+    param_preview.nPhoton = 10000
+    param_preview.itr = 100
+
+    param_final = composition.core.PPMParam()
+    param_final.nRay = 256
+    param_final.nPhoton = 100000
+    param_final.itr = 10000
+
+    param = param_preview
 
 
-param_preview = composition.core.PPMParam()
-param_preview.nRay = 16
-param_preview.nPhoton = 10000
-param_preview.itr = 100
 
-param_final = composition.core.PPMParam()
-param_final.nRay = 256
-param_final.nPhoton = 100000
-param_final.itr = 10000
+    time.sleep(0.2)
+    time0 = time.time()
+    #print('pt'); #cmp.pt_ref(rf, 500)
+    #print('pt_nt'); cmp.pt_nt(nt, 500)
 
-param = param_preview
+    # print('ppm ref'); cmp.ppm_ref(rf, param)
 
-print('genHits')
-hits0 = cmp.genHits_ex(0, param.nRay)
-hits1 = cmp.genHits_ex(1, param.nRay)
+    print('collect target hitpoints')
+    hits0 = cmp.genHits_ex(0, param.nRay)
+    hits1 = cmp.genHits_ex(1, param.nRay)
 
-print('radiance estimate')
-cmp.ppm_radiance(hits0, 0, param)
-hits0.save(path + "hits1_bump_flat_" + str(param.nRay))
+    print('radiance estimate')
+    cmp.ppm_radiance(hits0, 0, param)
+    hits0.save(path + "hits1_bump_flat_" + str(param.nRay))
 
-cmp.ppm_radiance(hits1, 1, param)
-hits1.save(path + "hits2_bump_flat_" + str(param.nRay))
+    cmp.ppm_radiance(hits1, 1, param)
+    hits1.save(path + "hits2_bump_flat_" + str(param.nRay))
 
-ramp0 = col.Ramp(ramp_red, 'const')
-ramp1 = col.Ramp(ramp_green, 'linear')
+    ramp0 = col.Ramp(ramp_red, 'const')
+    ramp1 = col.Ramp(ramp_green, 'linear')
 
-remap0 = col.basis.ramp(col.basis.sumRadianceRGB, ramp0.eval)
-remap1 = col.basis.ramp(col.basis.sumRadianceRGB, ramp1.eval)
+    remap0 = col.basis.ramp(col.basis.sumRadianceRGB, ramp0.eval)
+    remap1 = col.basis.ramp(col.basis.sumRadianceRGB, ramp1.eval)
 
-print('conversion')
-cmp.hitsToImage(hits0, t0, col.basis.radiance)
-cmp.hitsToImage(hits1, t1, col.basis.radiance)
+    print('conversion')
+    cmp.hitsToImage(hits0, t0, col.basis.radiance)
+    cmp.hitsToImage(hits1, t1, col.basis.radiance)
 
-print(time.time() - time0)
+    print(time.time() - time0)
 
-terminate()
+    terminate()
+    return
+
+main()
