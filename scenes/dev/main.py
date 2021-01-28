dc3652df4e5588c2fdca902c40a172531fb72c71
trnciii <doityan223@gmail.com>
2021-01-29 00:10:56 +0900

    update scene: for fig

--- scenes/dev/main.py
+++ scenes/dev/main.py
@@ -4,31 +4,16 @@ import composition
 col = composition.color
 
 # define ramps
-ramp_green0 = [
-    (0.07, [0.03, 0.1, 0.03]),
-    (0.3, [0.1, 0.5, 0.1 ]),
-    (0.5, [0.6, 0.8, 0.2]),
-    (0.8, [0.6, 0.8, 0.2]),
-    (1, [0.9, 1, 0.9])
-]
-
-ramp_green1 = [
-    (0.07, [0.03, 0.1, 0.03]),
-    (0.5, [0.1, 0.5, 0.1 ]),
-    (0.8, [0.6, 0.8, 0.2]),
-    (1, [0.9, 1, 0.9])
-]
-
-ramp_green2 = [
+ramp_green = [
     (0, [0.03, 0.1, 0.03]),
     (0.07, [0.1, 0.5, 0.1 ]),
     (0.3 , [0.6, 0.8, 0.2]),
     (0.8 , [0.9, 1, 0.9]),
 ]
 
-ramp_red0 = [
-    (0, [0.1, 0.02, 0.02]),
-    (0.25, [0.5, 0.4, 0.8]),
+ramp_red = [
+    (0, [0.2, 0.1, 0.1]),
+    (0.25, [0.5, 0.8, 0.4]),
     (0.3, [0.5, 0.1, 0.1]),
     (0.65, [0.8, 0.7, 0.2]),
     (1.5, [1, 1, 0.95])
@@ -36,8 +21,8 @@ ramp_red0 = [
 
 param_preview = composition.core.PPMParam()
 param_preview.nRay = 16
-param_preview.nPhoton = 1000
-param_preview.itr = 200
+param_preview.nPhoton = 50000
+param_preview.itr = 2000
 
 param_final = composition.core.PPMParam()
 param_final.nRay = 256
@@ -47,7 +32,7 @@ param_final.itr = 10000
 param = param_preview
 
 def target0():
-    ramp = col.RampData(ramp_green2, 'const')    
+    ramp = col.RampData(ramp_green, 'const')    
     # print(ramp)
     # remap = col.basis.ramp(col.basis.sumRadianceRGB, ramp)
     # ramp = 'ColorRamp'
@@ -61,22 +46,29 @@ def target0():
     return remap
 
 def target1():
-    # ramp = col.RampData(ramp_red0, 'const')
+    ramp = col.RampData(ramp_red, 'const')
     # print(ramp)
     # ramp = composition.bi.sliceImage('a.png', 0.5)
-    ramp = 'ColorRamp.001'
+#    ramp = 'ColorRamp.001'
+
+    def u(hit):
+        r = col.basis.sumRadianceRGB(hit)
+        r = r**0.5
+        r = r*1.4
+        return r
     
     composition.bi.rampToImage(targetMaterials[1]+'_texture', ramp, 256, 16)
 
-    remap = composition.bi.ramp(col.basis.sumRadianceRGB, ramp)
-    # remap = col.mix(remap, col.basis.radiance, 0.25)
-    # remap = col.mul(remap, col.basis.radiance)
-    # remap = col.mul(remap, col.basis.const(6, 6, 6))
+    remap = composition.bi.ramp(u, ramp)
+    remap = col.mul(remap, col.basis.radiance)
+    remap = col.mul(remap, col.basis.const(6, 6, 6))
+#    remap = col.mix(remap, col.basis.radiance, 0.3)
     
     return remap
 
 targetMaterials = ['target1', 'target2']
 targetRemap = [target0(), target1()]
+#targetRemap[1] = col.basis.radiance
 
 spheres = ['Sphere.001', 'Sphere']
 meshes = ['Sphere.002', 'floor', 'left', 'back', 'right']
@@ -109,6 +101,7 @@ def render():
     cmp.ppm_targets_ex(param)
 
     cmp.remapAll([col.basis.radiance]*len(cmp.targets))
+    print('-- end renderingz --')
 
 def remap():
     print('\033[36mremapping\033[0m')
@@ -126,5 +119,5 @@ def remap():
     print('-- end reamapping --')
     return
 
-render()
+#render()
 remap()
