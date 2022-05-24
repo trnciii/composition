c08ee9e0625af06edd608a6b9ea9e72d63a7f622
trnciii <doityan223@gmail.com>
2022-05-25 03:12:17 +0900

    utilize tuple

--- composition/color/RampData.py
+++ composition/color/RampData.py
@@ -12,6 +12,7 @@ class RampData():
 		self.mode = mode
 		self.data = pairs
 
+
 	@property
 	def mode(self):
 		return self._mode
@@ -23,50 +24,46 @@ class RampData():
 		else:
 			print("invalid mode")
 
+
 	@property
 	def data(self):
 		return self._data
 
 	@data.setter
 	def data(self, pairs):
-		pairs.sort()
-		self._data = []
-		for pair in pairs:
-			self._data.append((pair[0], np.array(pair[1])))
+		self._data = [(f, np.array(s)) for f, s in sorted(pairs)]
+
 
 	@property
 	def eval(self):
 		return self.evalTypes[self._mode]
 		
+
 	def evalConst(self, u):
 		c = self._data[0][1]
-		for pair in self._data:
-			if u>pair[0]: c = pair[1]
-		return [c[0], c[1], c[2]]
+		for first, second in self._data:
+			if u>first: c = second
+			else: return c
+		return c
+
 
 	def evalLinear(self, u):
-		if u<=self._data[ 0][0]:
-			c = self._data[0][1]
-			return [c[0], c[1], c[2]]
+		if u<=self._data[0][0]:
+			return self._data[0][1]
+
 		if u>=self._data[-1][0]:
-			c = self._data[-1][1]
-			return [c[0], c[1], c[2]]
+			return self._data[-1][1]
 		
-		x1 = self._data[0][0]
-		y1 = self._data[0][1]
-		
-		for pair in self._data[1:]:
-			x2 = pair[0]
-			y2 = pair[1]
-			
+
+		x1, y1 = self._data[0]
+
+		for x2, y2 in self._data[1:]:
 			if x1<=u and u<x2:
-				c = ((x2-u)*y1 + (u-x1)*y2)/(x2-x1)
-				return [c[0], c[1], c[2]]
+				return ((x2-u)*y1 + (u-x1)*y2)/(x2-x1)
+			x1, y1 = x2, y2
 
-			x1 = x2
-			y1 = y2
+		return np.zeros(3)
 
-		return [0, 0, 0]
 
 	def __str__(self):
 		s = "ramp <" + str(self._mode) + ">\n"
